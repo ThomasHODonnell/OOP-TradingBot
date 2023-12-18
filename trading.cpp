f46a49b2373304a2c449feacd4ef3fdd41211232
Thomas O'Donnell <thomasodonnellcollege@gmail.com>
2023-12-17 22:14:12 -0500

    Implement Trend Detection

--- trading.cpp
+++ trading.cpp
@@ -36,17 +36,20 @@ public:
 template <typename Y> class Strategy : public Purchase<Y> {
 private: 
     bool uptrend; 
-    Y currentPrice; 
     int strength; 
     vector<Y> rollingPrice; 
     vector<Y> highs;
     vector<Y> lows;  
 
 public: 
+    void setStrength(int s); 
+    void setTrend(bool trend); 
     void setTime(); 
     void setCurrent(Y current); 
     void setRolling(Y current); 
-    void higherHighs(); 
+    int getStrength();
+    bool getTrend(); 
+    bool higherHighs(); 
     void highOrLow(); 
     void priceHistory(); 
     void initializeRolling(Y first, Y second); 
@@ -103,24 +106,42 @@ template<typename X> void Purchase<X> :: sellStock() {
     return; 
 }
 
-template<typename Y> void Strategy<Y> :: setCurrent(Y current) { currentPrice = current; return;}
+template<typename Y> void Strategy<Y> :: setStrength(int s) { strength = s; return; }
+template<typename Y> void Strategy<Y> :: setTrend(bool trend) {uptrend = trend;}
 template<typename Y> void Strategy<Y> :: setRolling(Y current) { 
-    setCurrent(current); 
-    rollingPrice.push_back(currentPrice); 
+    this->setPrice(current); 
+    rollingPrice.push_back(this->getPrice()); 
     highOrLow(); 
     return;
     }
-template<typename Y> void Strategy<Y> :: higherHighs() {}
+template<typename Y> int  Strategy<Y> :: getStrength() { return strength; }
+template<typename Y> bool Strategy<Y> :: getTrend() { return higherHighs(); }
+template<typename Y> bool Strategy<Y> :: higherHighs() {
+    // let uptrend be 2 cycles
+    bool uptrend = false; 
+    int a = 0, b = a, c = a, d = a; 
+    if (highs[highs.size() - 1] == 0) {a = 2; b = 1; c = 4; d = 3;}
+    if (lows[lows.size() - 1] == 0) {a = 1; b = 2; c = 3; d = 4;}
+        if (highs[highs.size() - a] > highs[highs.size() - c]) {
+                 if (lows[lows.size() - b] > lows[lows.size() - d]) 
+                        setStrength(this->getStrength() + 1);
+        }
+        else {setStrength(this->getStrength() - 1);}
+        if (this->getStrength() > 1) uptrend = true;
+        if (this->getStrength() > 3) setStrength(3); 
+        else if (this->getStrength() < 0) setStrength(0); 
+        setTrend(uptrend);
+
+    return uptrend; 
+}
 template<typename Y> void Strategy<Y> :: highOrLow() {
-    if (currentPrice < rollingPrice.at(rollingPrice.size() - 2)) {
+    if (this->getPrice() < rollingPrice.at(rollingPrice.size() - 2)) {
         if (rollingPrice.at(rollingPrice.size() - 2) > rollingPrice.at(rollingPrice.size() - 3)) {
             highs.push_back(rollingPrice.at(rollingPrice.size() - 2)); 
             lows.push_back((Y) 0.00);
         }
-        }
-    
-
-    else if (currentPrice > rollingPrice.at(rollingPrice.size() - 2)) {
+    }
+    else if (this->getPrice() > rollingPrice.at(rollingPrice.size() - 2)) {
         if (rollingPrice.at(rollingPrice.size() - 2) < rollingPrice.at(rollingPrice.size() - 3)) {
             lows.push_back(rollingPrice.at(rollingPrice.size() - 2));
             highs.push_back((Y) 0.00);
@@ -131,14 +152,15 @@ template<typename Y> void Strategy<Y> :: highOrLow() {
     return;
 }
 template<typename Y> void Strategy<Y> :: priceHistory() {
-    cout << "------TIME------ROLLING------HIGHS------LOWS--------\n";
+    cout << "------TIME---------ROLLING---------HIGHS---------LOWS------TREND------\n";
     for (int i = 0; i < rollingPrice.size(); i++) {
         cout << "    " << this->getTime(); 
-        cout << "    " << setprecision(2) << fixed << rollingPrice[i]; 
+        cout << "      " << setprecision(2) << fixed << rollingPrice[i]; 
         cout << "\t     " << setprecision(2) << fixed << highs[i]; 
-        cout << "\t     " << setprecision(2) << fixed << lows[i] << endl;  
+        cout << "\t       " << setprecision(2) << fixed << lows[i];  
+        cout  << "\t " << this->getTrend() << endl;  
     }
-    cout << "----------------------------------------------------\n";
+    cout << "----------------------------------------------------------------------\n";
     return;
 }
 template<typename Y> void Strategy<Y> :: initializeRolling (Y first, Y second) {
@@ -148,39 +170,70 @@ template<typename Y> void Strategy<Y> :: initializeRolling (Y first, Y second) {
     highs.push_back((Y)0.00);
     lows.push_back((Y)0.00);
     lows.push_back((Y)0.00);
+    this->setStrength(0); 
     return;
 }
 int main () {
 
 // create stock data
 string sym = "BRK-B"; 
-float price = 355.56; 
+float price = 1000;
+float second = 1100; 
 
-// create instance, insert to stock list
-Strategy<float> BRKB;
-BRKB.insertNew(sym, price); 
-BRKB.printData(); 
+Strategy<float> BRKB; 
+BRKB.initializeRolling(price, second); 
 
-// buy stock
-BRKB.purchaseStock(price);
-
-// attempt to sell stock (would not make money) -> failed
-BRKB.sellStock(); 
-
-// redefine price -> sell for profit
-price = 400.43; 
-BRKB.setPrice(price); 
-BRKB.sellStock(); 
-
-// test strategy 
-//BRKB.setRolling(price); 
-float second = 350; 
-BRKB.initializeRolling(price, second);
-
-price = 405.00;
-BRKB.setRolling(price); 
+// price = 100; 
+// BRKB.setRolling(price);
+// price = 200; 
+// BRKB.setRolling(price);
+// price = 50; 
+// BRKB.setRolling(price);
+// price = 400; 
+// BRKB.setRolling(price);
+// price = 300; 
+// BRKB.setRolling(price);
+// price = 500; 
+// BRKB.setRolling(price);
+// price = 350; 
+// BRKB.setRolling(price);
 
 
+price = 1000; 
+BRKB.setRolling(price);
+
+price = 1100; 
+BRKB.setRolling(price);
+
+price = 800; 
+BRKB.setRolling(price);
+
+price = 900; 
+BRKB.setRolling(price);
+
+price = 500; 
+BRKB.setRolling(price);
+
+price = 600; 
+BRKB.setRolling(price);
+
+price = 550; 
+BRKB.setRolling(price);
+
+price = 750; 
+BRKB.setRolling(price);
+
+price = 700; 
+BRKB.setRolling(price);
+
+price = 900; 
+BRKB.setRolling(price);
+
+price = 850; 
+BRKB.setRolling(price);
+
+price = 1000; 
+BRKB.setRolling(price);
 
 
     return 0; 
